68840786dfbdbeea93a30207287e052846502a25
Lincoln Gadea <lincoln@glinc.com.br>
2021-04-28 08:22:58 -0300

    chore: cria entidade carro, gera os bytecodes do grpc, cria CarrosEndProint e CarroRepository

--- src/main/kotlin/io/kotlin/grpc/testes/carros/CarroRepository.kt
+++ src/main/kotlin/io/kotlin/grpc/testes/carros/CarroRepository.kt
@@ -4,6 +4,6 @@ import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
 
 @Repository
-class CarroRepository: JpaRepository<Carro, Long> {
-
+interface CarroRepository: JpaRepository<Carro, Long> {
+    fun existsByPlaca(placa: String?): Boolean
 }
--- src/main/kotlin/io/kotlin/grpc/testes/carros/CarrosEndPoint.kt
+++ src/main/kotlin/io/kotlin/grpc/testes/carros/CarrosEndPoint.kt
@@ -1,7 +1,39 @@
 package io.kotlin.grpc.testes.carros
 
+import io.grpc.Status
+import io.grpc.stub.StreamObserver
+import io.kotlin.grpc.testes.CarroRequest
+import io.kotlin.grpc.testes.CarroResponse
 import io.kotlin.grpc.testes.CarrosGrpcServiceGrpc
+import org.hibernate.exception.ConstraintViolationException
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class CarrosEndPoint(@Inject val repository: CarroRepository): CarrosGrpcServiceGrpc.CarrosGrpcServiceImplBase() {
+
+    override fun adicionar(request: CarroRequest, responseObserver: StreamObserver<CarroResponse>) {
+
+        if(repository.existsByPlaca(request.placa)){
+            responseObserver.onError(Status.ALREADY_EXISTS
+                .withDescription("carro com placa existente")
+                .asRuntimeException())
+            return
+        }
+
+        val carro = Carro(
+            modelo = request.modelo,
+            placa = request.placa
+        )
+
+        try {
+            repository.save(carro)
+        } catch(e: ConstraintViolationException){
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                .withDescription("dados de entrada inválidos")
+                .asRuntimeException())
+            return
+        }
+
+    }
 }
